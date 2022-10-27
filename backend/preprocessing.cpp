b2265aa2fca6036116c40eb145aeb9b963f7e1af
mattiadido95 <mattia.didonato95@gmail.com>
2022-10-27 09:13:30 +0200

    prepare merge with salvo

--- backend/preprocessing.cpp
+++ backend/preprocessing.cpp
@@ -7,7 +7,7 @@ using namespace std;
 vector<string> Preprocessing::stopwords()
 {
     string lineread;
-    ifstream sw("stopwords.txt");
+    ifstream sw("stopwords.txt"); // taken from https://gist.github.com/sebleier/554280
     vector<string> stopwords;
     while (sw.good())
     {
@@ -15,6 +15,8 @@ vector<string> Preprocessing::stopwords()
         stopwords.push_back(lineread);
     }
     sw.close();
+
+    // sort stopword for binary search
 
     return stopwords;
 }
