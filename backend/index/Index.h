4732699ccc2a0591fae1eec29e37a036eaba5b0b
mattiadido95 <mattia.didonato95@gmail.com>
2022-11-10 11:17:07 +0100

    fix post merge

--- backend/index/Index.h
+++ backend/index/Index.h
@@ -6,28 +6,33 @@ using namespace std;
 
 #ifndef BACKEND_INDEX_H
 #define BACKEND_INDEX_H
-struct post{
+struct post {
     string id;
     int tf;
 };
 
-struct term_info{
+struct term_info {
     int df; //number of doc that mantein this term
     int cf; //total number of occurency
     int start_ind;
     int end_ind;
     vector<post> *posting_list;
 };
+
 class Index {
 private:
 
 
 public:
     map<string, term_info> lexicon;
-    map<string,int> documentIndex;
-    void addDocIndex(string,int);
+    map<string, int> documentIndex;
+
+    void addDocIndex(string, int);
+
     int addLexicon(string, string);
-    void add_document_index(string,int);
+
+    void add_document_index(string, int);
+
     string docid(); //restituisce il docid del post corrente
     void next(); //sposta in sequenza l'iteratore al successivo
     void nextGEQ(string); //sposta l'iteratore al successivo con id del posting >=
