79a29b4f803fc8f15bf3d5fcf8476b884ae4fd0c
mattiadido95 <mattia.didonato95@gmail.com>
2022-11-11 17:33:34 +0100

    add stxxl

--- backend/index/Index.h
+++ backend/index/Index.h
@@ -1,17 +1,18 @@
 #include <string>
 #include <vector>
 #include <map>
+#include <stxxl>
 
 using namespace std;
 
 #ifndef BACKEND_INDEX_H
 #define BACKEND_INDEX_H
-struct post{
+struct post {
     string id;
     int tf;
 };
 
-struct term_info{
+struct term_info {
     int df; //number of doc that maintain this term
     int cf; //total number of occurrence
     int start_ind;
@@ -24,17 +25,22 @@ class Index {
 
 private:
     map<string, term_info> lexicon;
-    map<string,int> documentIndex;
+    map<string, int> documentIndex;
+    stxxl::vector<int> prova;
 public:
-    void addDocIndex(string,int);
+    void addDocIndex(string, int);
+
     int addLexicon(string, string);
-    void add_document_index(string,int);
+
+    void add_document_index(string, int);
+
     string docid(); //restituisce il docid del post corrente
     void next(); //sposta in sequenza l'iteratore al successivo
     void nextGEQ(string); //sposta l'iteratore al successivo con id del posting >=
 
-    map<string, term_info> getLexicon(){return lexicon;}
-    map<string,int> getDocumentIndex(){return documentIndex;}
+    map<string, term_info> getLexicon() { return lexicon; }
+
+    map<string, int> getDocumentIndex() { return documentIndex; }
 
 };
 
